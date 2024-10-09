857d71d9c4750725cac16b9d2ffc47066b632912
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-10-09 22:26:48 +0200

    new

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::io::{self, Write};
+// TODO: 
+// - enum of directions
+// - erase dots
+
+use std::cmp::min;
 use ncurses::*;
 
 struct Pyramid {
@@ -24,31 +28,83 @@ impl Pyramid {
 }
 
 fn main() {
-    let pyr = Pyramid::create_with(6, 'o');
+    let mut pyr = Pyramid::create_with(6, 'o');
 
     initscr();
     raw();
     noecho();
 
+    let mut pos = (0, 0);
+
     loop {
         draw(&pyr);
-        // refresh();
 
-        match getch() as u8 as char {
+        mv(pos.1, pos.0);
+
+        let key = getch() as u8 as char;
+        match key {
             'q' => break,
+            'c' => erase(pos, &mut pyr),
             _ => (),
         }
 
+        pos = move_curs(key, pos, &pyr);
+
+        refresh();
         clear();
+
+        // let coords = format!("{}, {}", cur_x, cur_y);
+        // addstr(&coords as &str).unwrap();
     }
 
     endwin();
+
+    // for (i, layer) in pyr.core.iter().enumerate() {
+    //     for (j, dot) in layer.iter().enumerate() {
+    //         print!("({j}, {i})");
+    //     }
+    //     println!("\n");
+    // }
+}
+
+fn erase(pos: (i32, i32), pyr: &mut Pyramid) {
+    pyr.core[pos.1 as usize][pos.0 as usize] = '-';
+}
+
+fn move_curs(key: char, mut pos: (i32, i32), pyr: &Pyramid) -> (i32, i32) {
+    match key {
+        'k' => {
+            if pos.1 > 0 {
+                pos.1 -= 1;
+            }
+            pos
+        },
+
+        'j' => {
+            pos.1 = min(pos.1 + 1, pyr.size as i32 - 1);
+            pos
+        },
+
+        'h' => {
+            if pos.0 > 0 {
+                pos.0 -= 1;
+            }
+            pos
+        },
+
+        'l' => {
+            pos.0 = min(pos.0 + 1, (pyr.core[pos.1 as usize].len() - 1).try_into().unwrap());
+            pos
+        },
+        
+        _ => pos
+    }
 }
 
 fn draw(pyr: &Pyramid) {
     for layer in pyr.core.iter() {
-        for dot in layer.iter() {
-            let dot_str = format!(" {} ", pyr.dot);
+        for _dot in layer.iter() {
+            let dot_str = format!("{}", pyr.dot);
             addstr(&dot_str as &str).unwrap();
         }
         addstr("\n").unwrap();
